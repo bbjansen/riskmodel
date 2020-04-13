943fbac7f1c8f1546902333ef7332d926b05a816
Olivier Brouwer <olivier.brouwer@vattenfall.com>
2020-04-13 17:27:59 +0200

    Load secret from env file

--- src/risk_model/storage.py
+++ src/risk_model/storage.py
@@ -1,3 +1,4 @@
+"""AWS data access."""
 
 from dotenv import load_dotenv
 import os
@@ -7,17 +8,9 @@ import re
 
 
 def with_aws_service(f):
-    """Decorate function to add aws service."""
+    """Decorate function to access aws service."""
     def wrap(*args, **kwargs):
         load_dotenv()
-        ACCESS_KEY = os.getenv('STORAGE_ACCOUNT_NAME')
-        SECRET_KEY = os.getenv('STORAGE_ACCOUNT_KEY')
-
-        client = boto3.client(
-            's3',
-            aws_access_key_id=ACCESS_KEY,
-            aws_secret_access_key=SECRET_KEY,
-        )
         s3 = boto3.resource('s3')
         kwargs['s3'] = s3
         return f(*args, **kwargs)
@@ -30,28 +23,31 @@ def make_folder(path):
         os.makedirs(path)
 
 
-def extract_filename_from_filepath(filepath):
+def extract_filename_from_filepath(filepath, file_type):
     """Extract filename from full filepath."""
-    return (
-        re
-        .search('/(.*).csv', filepath)
-        .group(1)
-        .split('/')
-    )[-1]
+    for file_type in file_type:
+        filename = re.search('/(.*).{}'.format(file_type), filepath)
+
+        if filename:
+            return filename.group(1).split('/')[-1] + ".{}".format(file_type)
 
 
 @with_aws_service
-def download_csv_files(
-    bucket: str, remote: str, local: str, *args, **kwargs
+def download_files_remote_to_local(
+    log, bucket: str,
+    remote: str, local: str, file_type: list, *args, **kwargs
 ):
+    """Downloads files from AWS bucket in remote folder and
+    puts them in the local folder."""
     make_folder(os.path.join(local, remote))
     s3 = kwargs['s3']
 
-    files = list_csv_files(bucket, remote)
+    files = list_files(bucket, remote, file_type)
 
     for file in files:
         try:
-            file_name = extract_filename_from_filepath(file)
+            log.info(f"Downloading: {file}")
+            file_name = extract_filename_from_filepath(file, file_type)
             local_filename = os.path.join(local, remote, file_name)
             s3.Bucket(bucket).download_file(file, local_filename)
         except botocore.exceptions.ClientError as e:
@@ -62,17 +58,19 @@ def download_csv_files(
 
 
 @with_aws_service
-def list_csv_files(
-    bucket: str, remote: str, *args, **kwargs
+def list_files(
+    bucket: str, remote: str, file_type: list, *args, **kwargs
 ) -> list:
-    """Returns list of csv files in remote folder."""
+    """Returns list of files in remote folder."""
     s3 = kwargs['s3']
+
     files = []
 
     my_bucket = s3.Bucket(bucket)
 
     for file in my_bucket.objects.all():
-        if remote in file.key and file.key.endswith('.csv'):
+        if file.key.startswith(remote) and any(
+                file.key.endswith('.{}'.format(ext))
+                for ext in file_type):
             files.append(file.key)
-
     return files
