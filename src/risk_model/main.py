0e5715e81be681421cf9e7f10901ff590bf65f12
Olivier Brouwer <olivier.brouwer@vattenfall.com>
2020-04-13 17:29:17 +0200

    Preprocess json to make reading in python possible

--- src/risk_model/main.py
+++ src/risk_model/main.py
@@ -2,11 +2,11 @@
 
 import argparse
 import logging
-import os
 import sys
+import shutil
 
 from dotenv import load_dotenv
-from risk_model import storage
+from risk_model import storage, preprocess
 
 format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
@@ -15,29 +15,78 @@ logging.basicConfig(stream=sys.stdout,
                     format=format)
 
 
+BUCKET_NAME = 'legalthings-datalake'
+AWS_FILEPATHS = {
+    'mongo/': ['csv', 'json']
+}
+
+
 def get_args():
     """Parse potential user arguments."""
     parser = argparse.ArgumentParser('riskmodel')
     return parser.parse_args()
 
 
-def run(log):
+def run(
+    log,
+    download=False,
+    parse_json=True
+):
     """Run job."""
-    log.info("Starting collect data")
 
-    args = get_args()
-    log.info(f"Parsing args: {args}")
+    # Step 1: Download files from AWS S3.
 
-    log.info("Loading env vars")
-    load_dotenv('env_file')
+    if download:
+        log.info("Starting collect data")
 
-    log.info(f"Downloading and saving latest files")
-    cfgs = storage.download_csv_files(
-        bucket='legalthings-datalake',
-        remote='datastudio/BVshareholders',
-        local='data')
+        args = get_args()
+        log.info(f"Parsing args: {args}")
 
-    log.info("Finished collecting data")
+        log.info("Loading env vars")
+        load_dotenv('env_file')
+
+        log.info(f"Downloading and saving latest files")
+
+        for remote, file_type in AWS_FILEPATHS.items():
+            log.info(f"Downloading: {remote}{file_type} files")
+            storage.download_files_remote_to_local(
+                log,
+                bucket=BUCKET_NAME,
+                remote=remote,
+                local='data',
+                file_type=file_type)
+            log.info(f"Finished download and saved {remote} in data folder")
+        log.info("Finished collecting data")
+
+    # Step 2: Parse JSON to readable and memorable format.
+
+    if parse_json:
+        log.info("Start parsing readable JSON files")
+
+        # Clear directory
+        clear_dir = 'data/preprocess/incorporation_processes/'
+        if os.path.exists(clear_dir):
+            shutil.rmtree('data/preprocess/incorporation_processes/')
+
+        # Parse big JSON from onliner to multiple, python readable JSON lines
+        preprocess.make_readable_json(
+            unprocessed_filename='data/mongo/incorporation-processes.json',
+            preprocessed_dir='data/preprocess/incorporation_processes/',
+            split_variabele='{"_id":'
+        )
+        log.info("Finished onliner to multiple, python readable JSON lines")
+
+        # Split big json file into separate, python memorable json files
+        preprocess.split_file_into_multiple_files(
+            directory='data/preprocess/incorporation_processes',
+            file_to_split='incorporation-processes.json',
+            new_sub_file_name='processes',
+            n_files=35,
+            file_type='json'
+        )
+        log.info("Finished splitting big json into separate files")
+
+    log.info("Finished job.")
     return 0
 
 
---
+++ src/risk_model/preprocess.py
@@ -0,0 +1,89 @@
+"""Preprocess JSON files."""
+
+from risk_model.storage import make_folder
+import os
+import json
+from itertools import chain, islice
+
+
+def make_readable_json(
+    unprocessed_filename: str = 'data/mongo/incorporation-processes.json',
+    preprocessed_dir: str = 'data/preprocess/incorporation_processes/',
+    split_variabele: str = '{"_id":',
+    chunk_size=4096
+):
+    """Reformat unreadable oneline JSON to python readable format."""
+    make_folder(preprocessed_dir)
+    processed_filename = os.path.join(
+        preprocessed_dir, unprocessed_filename.split('/')[-1])
+
+    with open(unprocessed_filename) as f_read:
+        with open(processed_filename, 'w') as f_write:
+            for chunk in each_chunk(f_read, chunk_size, split_variabele):
+                format_json = split_variabele + chunk
+                if format_json != split_variabele and len(format_json) > 0:
+                    try:
+                        reformated_json = json.loads(format_json)
+                        f_write.write('{}\n'.format(
+                            json.dumps(reformated_json)))
+                    except json.JSONDecodeError:
+                        print('Fail to parse')
+                        pass
+
+
+def each_chunk(
+    stream,
+    chunk_size: int,
+    separator: str
+):
+    """Separates the oneline JSON into multiple JSON lines."""
+    buffer = ''
+    while True:
+        chunk = stream.read(chunk_size)
+        if not chunk:
+            yield buffer
+            break
+
+        buffer += chunk
+        while True:
+            try:
+                part, buffer = buffer.split(separator, 1)
+            except ValueError:
+                break
+            else:
+                yield part
+
+
+def chunks(
+    iterable,
+    n
+):
+    "chunks(ABCDE,2) => AB CD E"
+    iterable = iter(iterable)
+    while True:
+        try:
+            yield chain([next(iterable)], islice(iterable, n - 1))
+        except StopIteration:
+            return
+
+
+def split_file_into_multiple_files(
+    directory='data/preprocess/incorporation_processes',
+    file_to_split='incorporation-processes.json',
+    new_sub_file_name='processes',
+    n_files=35,
+    file_type='json'
+):
+    "Splits big file into separate files."
+
+    file = os.path.join(directory, file_to_split)
+    num_lines = sum(1 for line in open(file))
+    n_lines = round(num_lines / n_files)
+
+    with open(file) as bigfile:
+        for i, lines in enumerate(chunks(bigfile, n_lines)):
+            file_split = os.path.join(
+                directory, 'processes_{}.{}'.format(i, file_type))
+
+            with open(file_split, 'w') as f:
+                f.writelines(lines)
